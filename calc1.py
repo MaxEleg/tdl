ca5c784813bbd50360f083f8da7e3479af95ab11
maxeleg <maximilien.elegbe@outlook.fr>
2018-04-24 14:49:19 +0200

    initial commit

---
+++ calc1.py
@@ -0,0 +1,127 @@
+# -----------------------------------------------------------------------------
+# calc.py
+#
+# A simple calculator with variables.
+# -----------------------------------------------------------------------------
+
+tokens = (
+    'NAME','NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE',
+    'LPAREN','RPAREN', 'SEMICOLON'
+    )
+
+# Tokens
+
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_SEMICOLON    = r';'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Ignored characters
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lex.lex()
+
+# Precedence rules for the arithmetic operators
+precedence = (
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS')
+    )
+
+
+
+
+
+def p_statement_expr(p):
+    '''statement : statement expression SEMICOLON
+                 | expression SEMICOLON'''
+    length = len(p)
+
+    if length == 4:
+        print(eval(p[2]))
+    elif length == 3:
+        print(eval(p[1]))
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if p[2] == '+'  : p[0] = ('+', p[1], p[3])
+    elif p[2] == '-': p[0] = ('-', p[1], p[3])
+    elif p[2] == '*': p[0] = ('*', p[1], p[3])
+    elif p[2] == '/': p[0] = ('/', p[1], p[3])
+
+
+def p_expression_uminus(p):
+    'expression : MINUS expression %prec UMINUS'
+    p[0] = -p[2]
+
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expression_name(p):
+    'expression : NAME'
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
+
+def p_error(p):
+    print("Syntax error at '%s'" % p.value)
+
+def eval(t):
+    if type(t) == tuple:
+        op=t[0]
+        a=t[1]
+        b=t[2]
+
+        if op == '+':  return eval(a) + eval(b)
+        elif op == '-':  return eval(a) - eval(b)
+        elif op == '*':  return eval(a) * eval(b)
+        elif op == '/':  return eval(a) / eval(b)
+    else:
+        return t;
+
+def printTree(t):
+    if type(t) == tuple:
+        print(t[0])
+        print("_" + t[0])
+        print("_" + t[1])
+
+import ply.yacc as yacc
+yacc.yacc()
+
+while True:
+    try:
+        s = input('calc > ')   # use input() on Python 3
+    except EOFError:
+        break
+    yacc.parse(s)
